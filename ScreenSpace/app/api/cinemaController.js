b2564f10928752c2934b5e8a5d925b947881daf6
Federigo Gelsi <fgelsi97@gmail.com>
2023-06-21 11:05:33 -0300

    Added logic to getHalls and getHallByName

--- ScreenSpace/app/api/cinemaController.js
+++ ScreenSpace/app/api/cinemaController.js
@@ -4,11 +4,13 @@ import {cinemas} from '../ui/mock/mockValues';
 export const createCinema = newCinema => cinemas.push(newCinema);
 
 export const getCinemaByName = cinemaName =>
+  // FIXME: Call backend API using axios
   getCinemas().filter(cinema =>
     cinema.name.toLowerCase().includes(cinemaName.toLowerCase()),
   );
 
 export const getCinemas = (movieId, ownerId) => {
+  // FIXME: Call backend API using axios
   if (ownerId) {
     return cinemas.filter(cinema => cinema.userId === ownerId);
   } else {
@@ -17,9 +19,11 @@ export const getCinemas = (movieId, ownerId) => {
 };
 
 export const getCinemaById = cinemaId =>
+  // FIXME: Call backend API using axios
   getCinemas().find(cinema => cinema.id === cinemaId);
 
 export const updateCinema = newCinema => {
+  // FIXME: Call backend API using axios
   cinemaToUpdate = getCinemaById(newCinema.id);
   if (cinemaToUpdate === undefined) {
     createCinema(newCinema);
@@ -33,20 +37,39 @@ export const updateCinema = newCinema => {
 };
 
 export const deleteCinema = cinemaId => {
+  // FIXME: Call backend API using axios
   console.log(
     'Deleted cinema: ',
     cinemas.splice(cinemas.indexOf(getCinemaById(cinemaId)), 1),
   );
 };
 
-export const createHall = (cinemaId, payload) => {};
-export const getHalls = cinemaId => {};
-export const getHallById = (cinemaId, hallId) => {};
-export const updateHall = (cinemaId, hallId, payload) => {};
-export const deleteHall = (cinemaId, hallId) => {};
+export const createHall = (cinemaId, payload) => {
+  // TODO: Implement createHall
+};
+export const getHalls = cinemaId => {
+  // FIXME: Call backend API using axios
+  return getCinemaById(cinemaId).halls;
+};
+export const getHallById = (cinemaId, hallId) => {
+  // TODO: Implement getHallById
+};
+export const getHallByName = (cinemaId, hallName) => {
+  // TODO: Implement getHallById
+  getHalls(cinemaId).filter(hall => hall.name.toLowerCase().includes(hallName.toLowerCase()))
+};
+export const updateHall = (cinemaId, hallId, payload) => {
+  // TODO: Implement updateHall
+};
+export const deleteHall = (cinemaId, hallId) => {
+  // TODO: Implement deleteHall
+};
 
-export const createShow = (cinemaId, hallId, payload) => {};
+export const createShow = (cinemaId, hallId, payload) => {
+  // TODO: Implement createShow
+};
 export const getShows = cinemaId => {
+  // FIXME: Call backend API using axios
   return getCinemaById(cinemaId).halls.flatMap(o =>
     o.cinemaShows.map(e => ({
       id: o.id,
@@ -58,10 +81,13 @@ export const getShows = cinemaId => {
     })),
   );
 };
-export const getShowById = (cinemaId, showId) => {};
-export const updateShow = (cinemaId, showId, payload) => {};
+export const getShowById = (cinemaId, showId) => {
+  // TODO: Call backend API using axios
+};
+export const updateShow = (cinemaId, showId, payload) => {
+  // TODO: Call backend API using axios
+};
 export const deleteShow = showId => {
   // TODO: Add logic to delete show
   console.log('Deleted show with id: ', showId);
-  
 };
