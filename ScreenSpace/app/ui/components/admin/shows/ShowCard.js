da57a74ed81c85cea48159ad2c8e178f1ba47372
Federigo Gelsi <fgelsi97@gmail.com>
2023-06-21 10:31:05 -0300

    Minor improvements

--- ScreenSpace/app/ui/components/admin/shows/ShowCard.js
+++ ScreenSpace/app/ui/components/admin/shows/ShowCard.js
@@ -4,6 +4,8 @@ import {Button, Card, Icon, Layout, Modal, Text} from '@ui-kitten/components';
 
 import I18n from '../../../../assets/strings/I18n';
 import TEXT_KEY from '../../../../assets/strings/TextKey';
+import {deleteShow, getShowById} from '../../../../api/cinemaController';
+import { useDispatch } from 'react-redux';
 
 const Header = props => (
   <Layout {...props}>
@@ -11,8 +13,7 @@ const Header = props => (
   </Layout>
 );
 
-
-const CardActionButton = (props) => {
+const CardActionButton = props => {
   return (
     <Layout style={styles.buttonContainer}>
       <Button
@@ -33,59 +34,77 @@ const CardActionButton = (props) => {
   );
 };
 
-const CardDeleteModal = (props) =>{
+const CardDeleteModal = props => {
   return (
     <Modal
-        visible={props.deleteModalVisible}
-        backdropStyle={styles.backdrop}
-        onBackdropPress={() => props.setDeleteModalVisible(false)}>
-        <Card disabled={true}>
-          <Text style={{textAlign:"center"}}>{I18n.t(TEXT_KEY.cinemaShows.deleteShowWarningMessage)}</Text>
-          <Layout
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-around',
-              marginTop: 16,
-            }}>
-            <Button
-              style={styles.footerControl}
-              status="basic"
-              onPress={() => props.setDeleteModalVisible(false)}>
-              CANCEL
-            </Button>
-            <Button
-              style={styles.footerControl}
-              status="danger"
-              onPress={() => props.deleteShow(props.show.id)}
-              accessoryLeft={<Icon name="trash-2-outline" />}>
-              DELETE
-            </Button>
-          </Layout>
-        </Card>
-      </Modal>
-  )
-}
+      visible={props.deleteModalVisible}
+      backdropStyle={styles.backdrop}
+      onBackdropPress={() => props.setDeleteModalVisible(false)}>
+      <Card disabled={true}>
+        <Text style={{textAlign: 'center'}}>
+          {I18n.t(TEXT_KEY.cinemaShows.deleteShowWarningMessage)}
+        </Text>
+        <Layout
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginTop: 16,
+          }}>
+          <Button
+            style={styles.footerControl}
+            status="basic"
+            onPress={() => props.setDeleteModalVisible(false)}>
+            CANCEL
+          </Button>
+          <Button
+            style={styles.footerControl}
+            status="danger"
+            onPress={() => props.deleteShow(props.show.id)}
+            accessoryLeft={<Icon name="trash-2-outline" />}>
+            DELETE
+          </Button>
+        </Layout>
+      </Card>
+    </Modal>
+  );
+};
 
-const ShowCard = ({show}) => {
+const ShowCard = ({show, navigation}) => {
   const [deleteModalVisible, setDeleteModalVisible] = React.useState(false);
 
-  const deleteShow = (showId) => {
-    setDeleteModalVisible(false)
-    console.info("Delete show: ", showId);
-    //TODO: Add logic to remove show
-  }
-  
-  const editShow = (showId) => {
-    console.info("Edit show:", showId);
+  const dispatch = useDispatch();
+
+  const handleDeleteShow = (showId) => {
+    setDeleteModalVisible(false);
+    deleteShow(showId);
+  };
+
+  const editShow = showId => {
+    console.info('Edit show:', showId);
     //TODO: Add logic to edit show
-  }
+    navigateEditCinema(showId)
+  };
+
+  const navigateEditCinema = (showId) => {
+    navigation.push('NewShow');
+    // dispatch(loadForm(getShowById(showId)));
+  };
 
   return (
-    <Card style={styles.card} header={<Header movieName={show.movieName} />}>
+    <Card style={styles.card} header={<Header movieName={show.cinemaShow.name} />}>
       <Layout style={styles.footerContainer}>
-        <Text>{show.hall}</Text>
-        <CardActionButton show={show} editShow={editShow} setDeleteModalVisible={setDeleteModalVisible} />
-        <CardDeleteModal show={show} deleteShow={deleteShow} setDeleteModalVisible={setDeleteModalVisible} deleteModalVisible={deleteModalVisible} />
+        <Text>{show.name}</Text>
+        <CardActionButton
+          show={show}
+          editShow={editShow}
+          setDeleteModalVisible={setDeleteModalVisible}
+        />
+        <CardDeleteModal
+          show={show}
+          deleteShow={handleDeleteShow}
+          setDeleteModalVisible={setDeleteModalVisible}
+          deleteModalVisible={deleteModalVisible}
+        />
       </Layout>
     </Card>
   );
